86a638ddc6c54587bd03295b2f3a13e93efd73cd
Paweł Drelich <drelich_pawel@o2.pl>
2021-12-12 17:56:26 +0100

    Initial commit. Project created.

---
+++ app/src/main/java/pl/cutter72/sem5/notatnik/MainActivity.kt
@@ -0,0 +1,11 @@
+package pl.cutter72.sem5.notatnik
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
