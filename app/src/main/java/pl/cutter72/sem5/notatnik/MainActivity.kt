3d9bacb7a7fd0e852e57e20f929c58734e4e0f7f
Paweł Drelich <drelich_pawel@o2.pl>
2021-12-12 17:59:12 +0100

    MainActivity binding created.

--- app/src/main/java/pl/cutter72/sem5/notatnik/MainActivity.kt
+++ app/src/main/java/pl/cutter72/sem5/notatnik/MainActivity.kt
@@ -2,10 +2,15 @@ package pl.cutter72.sem5.notatnik
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import pl.cutter72.sem5.notatnik.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
